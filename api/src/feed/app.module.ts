112fa5c50aa976e49434a74e74fba35f66afce7e
Wisdom Eleeza <whiy07@gmail.com>
2023-09-19 16:08:43 +0000

    configuring environment variables(3)

--- api/src/feed/app.module.ts
+++ api/src/feed/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { AppController } from '../app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { FeedModule } from './feed.module';
+import { FeedController } from './controllers/feed.controller';
 
 @Module({
   imports: [
@@ -12,12 +14,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       port: parseInt(<string>process.env.POSTGRES_PORT),
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE  ,
+      database: process.env.POSTGRES_DATABASE,
       autoLoadEntities: true,
       synchronize: true, // this should not be used in production
     }),
+    FeedModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController, FeedController],
   providers: [],
 })
 export class AppModule {}
